e09662ed49d29a9f56fe84e71be64d86f6f12875
techwithRamesh <60956788+techwithRamesh@users.noreply.github.com>
2020-03-25 18:07:46 +0530

    Update simple_password_generator.py

--- simple_password_generator.py
+++ simple_password_generator.py
@@ -6,8 +6,6 @@ def Generate_Password(num):
     password = ''
 
     for n in range(num):
-        x = random.randint(0, 9)  # only random numbers
-        x = random.randint(10, 34)  # lower case chars
         x = random.randint(0, 94)
         password += string.printable[x]
     return password
